cfbdf44d721a89554d54bd486a67f413edcd0321
Xiangyu Wan <info@globizora.com>
2025-09-08 03:38:51 -0700

    chore(eslint): declare console global in flat config

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+﻿/** @type {import('eslint').Linter.FlatConfig[]} */
+module.exports = [
+  { ignores: ['node_modules', 'coverage', 'dist'] },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        global: true, process: true, __dirname: true, __filename: true,
+        module: true, exports: true, require: true, console: true
+      }
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'no-console': 'off',
+      'prefer-const': 'warn',
+      'eqeqeq': ['warn','smart']
+    }
+  },
+  { files: ['**/*.{test,spec}.js'], rules: { 'no-undef': 'off' } }
+];
